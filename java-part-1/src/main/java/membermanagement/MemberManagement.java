de9e41d7f3dced382b47ce309db3b0dd55636731
seplaz99 <seonghyun40@gmail.com>
2026-06-10 17:04:29 +0900

    회원관리 프로그램 리팩토링

---
+++ java-part-1/src/main/java/membermanagement/MemberManagement.java
@@ -0,0 +1,115 @@
+package main.java.membermanagement;
+
+public class MemberManagement {
+
+    private Member[] members;
+    private int totalCnt;
+    private int memberCnt;
+
+    public MemberManagement(int num) {
+        this.totalCnt = num * 10;
+        this.members = new Member[totalCnt];
+        this.memberCnt = 0;
+    }
+
+    public int getMemberCnt() { return  memberCnt; }
+    public int getTotalCnt() { return totalCnt; }
+
+    public void addMember() {
+        if (memberCnt >= totalCnt) {
+            System.out.println("회원이 꽉 찼습니다.");
+            return;
+        }
+
+        Member newMember = Print.inputMemberInfo();
+
+        if (checkEmail(newMember.getEmail())) {
+            System.out.println("이미 존재하는 회원입니다.");
+            return;
+        }
+
+        members[memberCnt] = newMember;
+        memberCnt++;
+        System.out.println("회원 등록이 완료되었습니다.");
+    }
+
+    public void selectEmail(String email) {
+        for (int i = 0; i < memberCnt; i++) {
+            if (members[i].getEmail().equals(email)) {
+                printMember(members[i]);
+                return;
+            }
+        }
+        System.out.println("찾으시는 정보가 없습니다.");
+    }
+
+    public void selectName(String name) {
+        boolean found = false;
+        for (int i = 0; i < memberCnt; i++) {
+            if (members[i].getName().equals(name)) {
+                printMember(members[i]);
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("찾으시는 정보가 없습니다.");
+        }
+    }
+
+    public void selectAll() {
+        if (memberCnt == 0) {
+            System.out.println("저장된 회원 정보가 없습니다.");
+            return;
+        }
+        for (int i = 0; i < memberCnt; i++) {
+            printMember(members[i]);
+        }
+    }
+
+    public void updateMember(String targetEmail) {
+        for (int i = 0; i < memberCnt; i++) {
+            if (members[i].getEmail().equals(targetEmail)) {
+                Member newInfo = Print.inputMemberInfo();
+
+                if (!targetEmail.equals(newInfo.getEmail()) && checkEmail(newInfo.getEmail())) {
+                    System.out.println("이미 존재하는 회원입니다.");
+                    return;
+                }
+
+                members[i].updateInfo(newInfo.getName(), newInfo.getEmail(), newInfo.getPhoneNumber());
+                System.out.println("회원 정보가 수정되었습니다.");
+                return;
+            }
+        }
+        System.out.println("수정하고자 하는 회원이 없습니다.");
+    }
+
+    public void deleteMember(String targetEmail) {
+        for (int i = 0; i < memberCnt; i++) {
+            if (members[i].getEmail().equals(targetEmail)) {
+                for (int j = i; j < memberCnt - 1; j++) {
+                    members[j] = members[j + 1];
+                }
+                members[memberCnt - 1] = null;
+                memberCnt--;
+
+                System.out.println("회원 정보가 삭제되었습니다.");
+                return;
+            }
+        }
+        System.out.println("삭제하고자 하는 회원이 없습니다.");
+    }
+
+    private boolean checkEmail(String email) {
+        for (int i = 0; i < memberCnt; i++) {
+            if (members[i].getEmail().equals(email)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void printMember(Member m) {
+        System.out.println("[이름] " + m.getName() + ", [이메일] " + m.getEmail() + ", [연락처] " + m.getPhoneNumber());
+    }
+}
