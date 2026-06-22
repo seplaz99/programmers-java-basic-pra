53aa6cf14437f91fe02421d05051e267b0c284df
seplaz99 <seonghyun40@gmail.com>
2026-06-22 10:48:50 +0900

    spring_theory

--- spring-theory/src/main/java/org/example/springtheory/ch01/ex1_2/dao/UserDAO_2.java
+++ spring-theory/src/main/java/org/example/springtheory/ch01/ex1_2/dao/UserDAO_2.java
@@ -8,21 +8,22 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 // * 문제점
-// 단지 커넥션 객체를 가져오는 방법을 분리하기 위해 상속 구조를 만들어버리면,
-// 후에 다른 목적으로 UserDAO에 상속을 적용하기 힘들다.
-// 또 다른 문제는 상속을 통한 상하위 클래스의 관계는 생각보다 밀접하다는 점이다.
-// 상속 관계는 두 가지 다른 관심사에 대해 긴밀한 결합을 허용한다.
-// 서브
+// 
 
+public abstract class UserDAO_2 {
 
-public abstract class UserDAO_3 {
+    private SimpleConnectionMaker simpleConnectionMaker;
+
+    public UserDAO_2() {
+        simpleConnectionMaker = new SimpleConnectionMaker();
+    }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
 
         String query = "INSERT INTO users (id, name, password) VALUES (?, ?, ?)";
 
         try (
-                Connection conn = getConnection();
+                Connection conn = simpleConnectionMaker.makeNewConnection();
                 PreparedStatement pstmt = conn.prepareStatement(query);
         ) {
             pstmt.setString(1, user.getId());
@@ -37,7 +38,7 @@ public abstract class UserDAO_3 {
         String query = "SELECT * FROM users WHERE id = ?";
 
         try (
-                Connection conn = getConnection();
+                Connection conn = simpleConnectionMaker.makeNewConnection();
                 PreparedStatement pstmt = conn.prepareStatement(query);
         ) {
             pstmt.setString(1, id);
@@ -55,9 +56,10 @@ public abstract class UserDAO_3 {
 
     }
 
-    // UserDAO의 소스코드를 제공하면, getConnection() 메서드를 원하는 방식으로 확장한 후
+    // UserDAO의 소스코드를 제공하면, getConnection() 메서드를 원하는 방식으로 확장한후
     // UserDAO의 기능과 함께 사용할 수 있다.
     // 기존에는 같은 클래스에 다른 메서드로 분리됐던 DB 커넥션 연결이라는 관심을
     // 이번에는 상속을 통해 서브클래스로 분리해버리는 것이다.
     public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
+
 }
