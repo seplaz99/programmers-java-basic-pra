11b34303ac82a8e7100d9f12f0efc7eeb71f1645
seplaz99 <seonghyun40@gmail.com>
2026-06-22 09:08:38 +0900

    spring theory prj setup

---
+++ spring-theory/src/main/java/org/example/springtheory/SpringTheoryApplication.java
@@ -0,0 +1,13 @@
+package org.example.springtheory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTheoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTheoryApplication.class, args);
+    }
+
+}
