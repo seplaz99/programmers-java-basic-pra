a5856bc3511170cc0212e63173f4450dbc0a0cb1
seplaz99 <seonghyun40@gmail.com>
2026-06-26 14:16:19 +0900

    spring_theory_ex3_3

--- spring-theory/src/main/java/org/example/springtheory/ch03/ex3_3/dao/UserDAO_2.java
+++ spring-theory/src/main/java/org/example/springtheory/ch03/ex3_3/dao/UserDAO_2.java
@@ -6,16 +6,16 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-// 로컬 클래스
-public class UserDAO {
+// 익명 내부 클래스
+public class UserDAO_2 {
 
     private SimpleConnectionMaker simpleConnectionMaker;
 
-    public UserDAO(SimpleConnectionMaker simpleConnectionMaker) {
+    public UserDAO_2(SimpleConnectionMaker simpleConnectionMaker) {
         this.simpleConnectionMaker = new DConnectionMaker();
     }
 
-    protected UserDAO() {}
+    protected UserDAO_2() {}
 
     public void jdbcContextWithStatementStrategy(StatementStrategy statementStrategy) throws SQLException, ClassNotFoundException {
         try (
@@ -28,7 +28,7 @@ public class UserDAO {
 
     public void add(User user) throws ClassNotFoundException, SQLException {
 
-        class UserDAOAdd implements StatementStrategy {
+        StatementStrategy strategy = new StatementStrategy() {
             @Override
             public PreparedStatement makeStatement(Connection conn) throws SQLException {
                 PreparedStatement pstmt = conn.prepareStatement("INSERT INTO users (id, name, password) VALUES (?, ?, ?)");
@@ -39,22 +39,22 @@ public class UserDAO {
 
                 return pstmt;
             }
-        }
+        };
 
-        StatementStrategy strategy = new UserDAOAdd();;
         jdbcContextWithStatementStrategy(strategy);
     }
 
     // 테스트 시작전에 호출해 DB를 깨씃한 상태로 만드는 용도
     public void deleteAll() throws SQLException, ClassNotFoundException {
 
-        class UserDAODeleteAll implements StatementStrategy {
+        StatementStrategy strategy = new StatementStrategy() {
+
             @Override
             public PreparedStatement makeStatement(Connection conn) throws SQLException {
                 return conn.prepareStatement("delete from users");
             }
-        }
+        };
 
-        jdbcContextWithStatementStrategy(new UserDAODeleteAll());
+        jdbcContextWithStatementStrategy(strategy);
     }
 }
