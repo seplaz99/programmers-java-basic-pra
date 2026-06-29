33486bf5737fbb04db3aa3c156b1e0b1d1bca31b
seplaz99 <seonghyun40@gmail.com>
2026-06-29 11:37:44 +0900

    spring_theory_ex5_1

--- spring-theory/src/main/java/org/example/springtheory/ch05/ex5_1/dao/UserDAO.java
+++ spring-theory/src/main/java/org/example/springtheory/ch05/ex5_1/dao/UserDAO.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 // 익명 내부 클래스
 public class UserDAO {
@@ -25,21 +26,29 @@ public class UserDAO {
             user.setId(rs.getString("id"));
             user.setName(rs.getString("name"));
             user.setPassword(rs.getString("password"));
-            user.setLevel(rs.get);
-            return null;
+            user.setLevel(Level.valueOf(rs.getInt("level")));
+            user.setLogin(rs.getInt("login"));
+            user.setRecommend(rs.getInt("recommend"));
+
+            return user;
         }
-    }
+    };
 
     public void add(User user) throws ClassNotFoundException, SQLException {
 
         StatementStrategy strategy = new StatementStrategy() {
             @Override
             public PreparedStatement makeStatement(Connection conn) throws SQLException {
-                PreparedStatement pstmt = conn.prepareStatement("INSERT INTO users (id, name, password) VALUES (?, ?, ?)");
+                PreparedStatement pstmt = conn.prepareStatement(
+                        "INSERT INTO users (id, name, password, level, login, recommend) VALUES (?, ?, ?, ?, ?)"
+                );
 
                 pstmt.setString(1, user.getId());
                 pstmt.setString(2, user.getName());
                 pstmt.setString(3, user.getPassword());
+                pstmt.setInt(4, user.getLevel().getValue());
+                pstmt.setInt(5, user.getLogin());
+                pstmt.setInt(6, user.getRecommend());
 
                 return pstmt;
             }
@@ -56,6 +65,69 @@ public class UserDAO {
             @Override
             public PreparedStatement makeStatement(Connection conn) throws SQLException {
                 return conn.prepareStatement("delete from users");
+            }
+        };
+
+        jdbcContext.workWithStatementStrategy(strategy);
+    }
+
+    public User get(String id) throws SQLException, ClassNotFoundException {
+        StatementStrategy strategy = new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection conn) throws SQLException {
+                PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM users WHERE id = ?");
+                pstmt.setString(1, id);
+                return pstmt;
+            }
+        };
+
+        return jdbcContext.queryForObject(strategy, userRowMapper);
+    }
+
+    public List<User> getAll() throws SQLException, ClassNotFoundException {
+        StatementStrategy strategy = new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection conn) throws SQLException {
+                PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM users ORDER BY id");
+                return pstmt;
+            }
+        };
+
+        return jdbcContext.query(strategy, userRowMapper);
+    }
+
+    public int getCount() throws SQLException, ClassNotFoundException {
+        StatementStrategy strategy = new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection conn) throws SQLException {
+                PreparedStatement pstmt = conn.prepareStatement("SELECT COUNT(*) FROM users");
+                return pstmt;
+            }
+        };
+
+        return jdbcContext.queryForObject(strategy, new RowMapper<>() {
+            @Override
+            public Integer mapRow(ResultSet rs) throws SQLException {
+                rs.next();
+                return rs.getInt(1);
+            }
+        });
+    }
+
+    public void uqdate(User user) throws SQLException, ClassNotFoundException {
+        StatementStrategy strategy = new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection conn) throws SQLException {
+                PreparedStatement pstmt = conn.prepareStatement(
+                        "UPDATE users SET name = ?, password = ?, level = ?, login = ?, recommend = ? WHERE id = ?");
+                pstmt.setString(1, user.getName());
+                pstmt.setString(2, user.getPassword());
+                pstmt.setInt(3, user.getLevel().getValue());
+                pstmt.setInt(4, user.getLogin());
+                pstmt.setInt(5, user.getRecommend());
+                pstmt.setString(6, user.getId());
+
+                return pstmt;
             }
         };
 
