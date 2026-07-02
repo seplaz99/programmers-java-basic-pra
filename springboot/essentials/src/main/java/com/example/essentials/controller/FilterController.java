78e8a49f2fa608d3c03eba05ebb9c04ef8cee052
seplaz99 <seonghyun40@gmail.com>
2026-07-02 12:17:26 +0900

    essentials

---
+++ springboot/essentials/src/main/java/com/example/essentials/controller/FilterController.java
@@ -0,0 +1,10 @@
+package com.example.essentials.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+// 디스패처 서블릿(DispatcherServlet)
+// @Controller와 RestController의 차이
+
+@RestController
+public class FilterController {
+}
