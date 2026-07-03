b1da6dd643897ec7c94ac1542185a2b3c4b90cdf
seplaz99 <seonghyun40@gmail.com>
2026-07-03 11:20:55 +0900

    feign client

--- springboot/feign/feign-client/src/main/java/com/example/feign_client/service/ExampleService.java
+++ springboot/feign/feign-client/src/main/java/com/example/feign_client/service/ExampleService.java
@@ -4,12 +4,14 @@ import com.example.feign_client.client.ExampleClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-
-@Service
 // @Component와 기능은 똑같다.
-@RequiredArgsConstructor
+@Service
 // final이 붙은 필드의 생성자를 만들어줌
+@RequiredArgsConstructor
 public class ExampleService {
     private final ExampleClient exampleClient;  // final을 붙인 이유 : 생성자 주입을 강제하기 위해서
-    
+
+    public String getDataById(Long id) {
+        return exampleClient.getData(id);
+    }
 }
