a05d71933d6a2c6c250f59261c42b57241715f2a
seplaz99 <seonghyun40@gmail.com>
2026-07-07 10:38:18 +0900

    basic board

---
+++ springboot/board/basic-board/src/main/java/com/example/basic_board/domain/repository/BoardRepository.java
@@ -0,0 +1,8 @@
+package com.example.basic_board.domain.repository;
+
+import com.example.basic_board.domain.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    
+}
