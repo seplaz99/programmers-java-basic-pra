a9005a331a8051fb2f0c48c6284fe8d6590d058b
seplaz99 <seonghyun40@gmail.com>
2026-07-07 10:35:11 +0900

    basic board

---
+++ springboot/board/basic-board/src/main/java/com/example/basic_board/domain/entity/Board.java
@@ -0,0 +1,36 @@
+package com.example.basic_board.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "board")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false, length = 50)
+    private String userId;
+
+    @Column(length = 255)
+    private String filePath;
+
+    // original : 2026-01-01T00:00:00
+    // -> 2026-01-01 00:00
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime created;
+}
