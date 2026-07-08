89e72c6fa6d1e55d578271bcd133c499ff9de2b6
seplaz99 <seonghyun40@gmail.com>
2026-07-08 10:18:34 +0900

    basic board detail

--- springboot/board/basic-board/src/main/java/com/example/basic_board/controller/BoardController.java
+++ springboot/board/basic-board/src/main/java/com/example/basic_board/controller/BoardController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BoardController {
@@ -18,6 +19,17 @@ public class BoardController {
     public String write(HttpSession session, Model model) {
         setSession(session, model);
         return "/board-write";
+    }
+
+    @GetMapping("/detail")
+    public String detail(
+            @RequestParam("id") Long id,
+            HttpSession session,
+            Model model
+    ) {
+        setSession(session, model);
+        model.addAttribute("id", id);
+        return "/board-detail";
     }
 
     private void setSession(HttpSession session, Model model) {
