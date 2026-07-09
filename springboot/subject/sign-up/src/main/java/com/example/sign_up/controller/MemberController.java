b81bc22b95377e6999f45229f958b809ab25e52e
seplaz99 <seonghyun40@gmail.com>
2026-07-09 12:34:21 +0900

    logout

--- springboot/subject/sign-up/src/main/java/com/example/sign_up/controller/MemberController.java
+++ springboot/subject/sign-up/src/main/java/com/example/sign_up/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.example.sign_up.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,5 +16,12 @@ public class MemberController {     // 화면 이동만 담당
     @GetMapping("/login")
     public String login() {
         return "sign-in";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+
+        return "redirect:/members/login";
     }
 }
