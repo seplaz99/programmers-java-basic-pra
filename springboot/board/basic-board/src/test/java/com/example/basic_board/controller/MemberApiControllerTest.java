451ca916d8f0c8cbb1ed37685613c1a9b3060632
seplaz99 <seonghyun40@gmail.com>
2026-07-09 10:39:03 +0900

    basic board test

---
+++ springboot/board/basic-board/src/test/java/com/example/basic_board/controller/MemberApiControllerTest.java
@@ -0,0 +1,7 @@
+package com.example.basic_board.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberApiControllerTest {
+
+}
---
+++ springboot/board/basic-board/src/test/java/com/example/basic_board/domain/repository/MemberRepositoryTest.java
@@ -0,0 +1,7 @@
+package com.example.basic_board.domain.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+}
