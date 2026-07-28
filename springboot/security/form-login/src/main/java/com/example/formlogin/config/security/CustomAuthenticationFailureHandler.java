052853ca82012983e236f095161d63780ce306b6
seplaz99 <seonghyun40@gmail.com>
2026-07-28 14:18:16 +0900

    spring-security form-login

--- springboot/security/form-login/src/main/java/com/example/formlogin/config/security/CustomAuthenticationFailureHandler.java
+++ springboot/security/form-login/src/main/java/com/example/formlogin/config/security/CustomAuthenticationFailureHandler.java
@@ -21,7 +21,7 @@ public class CustomAuthenticationFailureHandler implements AuthenticationFailure
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
 
-        SignInResponseDto.builder()
+        SignInResponseDto build = SignInResponseDto.builder()
                 .isLoggedIn(false)
                 .message("로그인 실패 \n 다시 로그인해주세요.")
                 .url("/users/login")
@@ -29,6 +29,6 @@ public class CustomAuthenticationFailureHandler implements AuthenticationFailure
 
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json;charset=UTF-8");
-        response.getWriter().write( objectMapper.writeValueAsString(SignInResponseDto.builder().build()) );
+        response.getWriter().write( objectMapper.writeValueAsString(build) );
     }
 }
