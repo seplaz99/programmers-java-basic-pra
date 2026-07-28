a55c476cf5bd4b8f80230118075f8b044b5cbfb4
seplaz99 <seonghyun40@gmail.com>
2026-07-28 10:43:21 +0900

    spring-security form-login

--- springboot/security/form-login/src/main/java/com/example/formlogin/config/security/CustomAuthenticationSuccessHandler.java
+++ springboot/security/form-login/src/main/java/com/example/formlogin/config/security/CustomAuthenticationSuccessHandler.java
@@ -39,6 +39,11 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
 
         response.setStatus(HttpServletResponse.SC_OK);
         response.setContentType("application/json;charset=UTF-8");
+
+        // objectMapper.writeValueAsString()으로 DTO를 JSON 문자열로 "직접" 변환해야한느 이유
+        // 이 핸들러는 security 필터 안에서 실행된다. 즉 DispatcherServlet에 도달하기 전 단계라서,
+        // 컨트롤러에서 쓰던 @RespnseBody + HttpMessageConverter의 자동 직렬화(객체 -> JSON)가 여기서는 등작하지 않는다.
+        //Jackson(ObjectMapper)으로 직렬화를 직접 수행한 뒤 응답 본문에 써준다.
         response.getWriter().write(objectMapper.writeValueAsString(build));
     }
 }
