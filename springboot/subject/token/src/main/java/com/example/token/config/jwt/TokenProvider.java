bbe856be46800380906073b902ed051823e6f497
seplaz99 <seonghyun40@gmail.com>
2026-07-29 11:40:25 +0900

    spring-security token

--- springboot/subject/token/src/main/java/com/example/token/config/jwt/TokenProvider.java
+++ springboot/subject/token/src/main/java/com/example/token/config/jwt/TokenProvider.java
@@ -1,5 +1,6 @@
 package com.example.token.config.jwt;
 
+import com.example.token.config.security.CustomUserDetails;
 import com.example.token.domain.entity.Role;
 import com.example.token.domain.entity.User;
 import io.jsonwebtoken.Claims;
@@ -10,6 +11,8 @@ import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -99,5 +102,13 @@ public class TokenProvider {
         return jwtParser.parseSignedClaims(token).getPayload();
     }
 
-    
+    // 복원된 User로 인증 정보를 만드는 메서드
+    public Authentication getAuthentication(User user, String token) {
+        CustomUserDetails principal = CustomUserDetails.builder()
+                .user(user)
+                .build();
+
+        return new UsernamePasswordAuthenticationToken(principal, token, principal.getAuthorities());
+
+    }
 }
